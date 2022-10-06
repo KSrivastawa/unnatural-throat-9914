13624c266072262608dbf1a6a456bebeccdfd797
KSrivastawa <kt.ketansri@gmail.com>
2022-10-06 20:43:53 +0530

    Tender management system

--- Administrator_DAO.java
+++
@@ -1,7 +0,0 @@
-package com.ketan.dataBase.object;
-
-public interface Administrator_DAO {
-
-	public String registerAdmin(int id, String name, String date, String email, String password);
-	
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-# unnatural-throat-9914
-
-# Project Name: Tender Management System
